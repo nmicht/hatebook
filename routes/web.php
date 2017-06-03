1ef8beef61bd1812b8c248e5fc1384571ace6ba9
Michelle Torres <michelle.torres@corb.mx>
2017-06-03 12:40:50 -0500

    Funcionalidad para crear un post y agrega campos fillable

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', 'PostController@index');
+Route::get('posts', 'PostController@index');
+Route::post('posts', 'PostController@store');
