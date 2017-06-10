ca6e92a1c149c7f4f2019d80da6d7ac729fcb4c5
Michelle Torres <michelle.torres@corb.mx>
2017-06-10 11:52:02 -0500

    Agrego campo de busqueda para listado

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,19 @@
 
 <div class="panel-heading">Todos tus pinches posts</div>
 <div class="panel-body">
+    <form class="form-horizontal" role="form" method="POST" action="{{ route('posts.index') }}">
+        <div class="form-group">
+            <label for="search" class="col-md-4 control-label">Qué pinches buscas?</label>
+            <div class="col-md-6">
+                <input id="search" type="search" class="form-control" name="search" value="{{ old('search') }}" required autofocus>
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">
+                    Buscar
+                </button>
+            </div>
+        </div>
+    </form>
     <ul>
     @forelse ($posts as $post)
         <li>
