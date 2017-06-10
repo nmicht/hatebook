6893d3338ddc71256eab048378a3e822d5f78f20
Michelle Torres <michelle.torres@corb.mx>
2017-06-10 13:17:58 -0500

    Crea un factory para posts usando faker

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,7 +12,7 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
@@ -20,5 +20,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Post::class, function(Faker\Generator $faker){
+    return [
+        'content' => $faker->text(200),
+        'user_id' => rand(1,4)
     ];
 });
