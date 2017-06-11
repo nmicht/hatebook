1401dd2202c0ebe1944064e25aeebb24e1ee3257
Michelle Torres <michelle.torres@corb.mx>
2017-06-11 11:47:41 -0500

    En la ruta de posts se pone a usar el middleware de usuarios baneados

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('posts', 'PostController');
+Route::resource('posts', 'PostController', [
+    'middleware' => 'App\Http\Middleware\BannedUsers',
+]);
 
 Route::get('interactions', 'UserController@interactions')->name('interactions');
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
