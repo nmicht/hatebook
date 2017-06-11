5fe7269dff83b901f5a418fdabeee5e1d3481b27
Michelle Torres <michelle.torres@corb.mx>
2017-06-11 11:35:01 -0500

    Agrego vistas de errores

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel-heading">NO ESTES MAMANDO</div>
+<div class="panel-body">
+    <h1>
+    Eres un don nadie. No tienes permisos para esta acción.
+    </h1>
+</div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel-heading">¿QUÉ PINCHES BUSCAS?</div>
+<div class="panel-body">
+    <h1>
+    La estas cagando. Eso que intentas ver no existe.
+    </h1>
+</div>
+@endsection
